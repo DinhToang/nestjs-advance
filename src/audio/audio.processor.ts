0c6632df40e83d70a54b0daf306480bce8bdf215
Ashe <you@example.comtoannguyen03.arc@gmail.com>
2025-12-04 13:27:42 +0700

    Queues

---
+++ src/audio/audio.processor.ts
@@ -0,0 +1,15 @@
+import { Process, Processor } from '@nestjs/bull';
+import { Logger } from '@nestjs/common';
+import type { Job } from 'bull';
+
+@Processor('audio-queue')
+export class AudioProcessor {
+  private logger = new Logger(AudioProcessor.name);
+  @Process('convert')
+  handleConvert(job: Job) {
+    console.log('Start converting wav file to mp3');
+    this.logger.debug('Start converting wav file to mp3');
+    this.logger.debug(job.data);
+    this.logger.debug('file converted succesfully');
+  }
+}
